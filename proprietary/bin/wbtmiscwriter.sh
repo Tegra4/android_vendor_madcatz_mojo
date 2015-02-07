bd17b60feb4362826b6c432c9f40615bf545e81c
c457 <android.c357@gmail.com>
2015-02-06 19:25:01 -0600

    mojo: remove ethernet_loader
    Need to find a way to set random mac without module dependency

--- proprietary/bin/wbtmiscwriter.sh
+++ proprietary/bin/wbtmiscwriter.sh
@@ -7,9 +7,3 @@ chmod 777 /data/misc/wifi/conf/wifi_famp_addr.conf
 #mft_data -w BT_MAC FEDCBA987654
 mft_data -r BT_MAC >> /data/misc/bluedroid/conf/bt_famp_addr.conf
 chmod 777 /data/misc/bluedroid/conf/bt_famp_addr.conf
-
-#mft_data -w WIFI_REG US39
-#mft_data -w WIFI_REG JP01
-#mft_data -w WIFI_REG XV01
-mft_data -r WIFI_REG >> /data/misc/wifi/conf/wifi_region.conf
-chmod 777 /data/misc/wifi/conf/wifi_region.conf
